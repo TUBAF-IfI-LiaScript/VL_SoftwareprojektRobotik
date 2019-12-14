48c15ab76db34aaa30dc8e6feaa34647f3fac745
Sebastian Zug <Sebastian.Zug@informatik.tu-freiberg.de>
2019-12-14 17:07:02 +0100

    Examplary service implementation completed

--- examples/08_ROS2_Kommunikation/ws_service/src/my_services_tutorial/src/service.cpp
+++ examples/08_ROS2_Kommunikation/ws_service/src/my_services_tutorial/src/service.cpp
@@ -1,21 +1,63 @@
 #include <iostream>
 #include <memory>
+#include <string>
+#include <vector>
 #include "rclcpp/rclcpp.hpp"
 #include "my_services/srv/robot_availability.hpp"
 
 using RobotAvailability = my_services::srv::RobotAvailability;
 rclcpp::Node::SharedPtr g_node = nullptr;
 
+struct Robot{
+    public:
+        int roomNumber;
+        std::string name;
+        float batterylevel;
+        bool busy;
+    public:
+        //default constructor
+        Robot(int number, std::string name, float battery, bool busy){
+            roomNumber=number; name=name; battery=battery; busy=busy;}
+};
+
+
+std::string locations[4] = { "Kitchen", "Livingroom", "Bedroom", "Batroom"};
+
+std::vector<Robot> RobotDB {
+    Robot(0, "Erika", 11.232, true),
+    Robot(1, "Maja", 12.0, false),
+    Robot(2, "Julius", 4.0, true),
+    Robot(3, "Alexander", 9.8, false),
+};
+
 void handle_service(
   const std::shared_ptr<rmw_request_id_t> request_header,
   const std::shared_ptr<RobotAvailability::Request> request,
   const std::shared_ptr<RobotAvailability::Response> response)
 {
+  bool gotIt = false;
   (void)request_header;
   RCLCPP_INFO(
     g_node->get_logger(),
-    "request: %s", request->robotname);
-  response->location = "Kitchen";
+    "Received request for : %s", request->robotname.c_str());
+  for (auto const& robot: RobotDB){
+    RCLCPP_INFO(
+      g_node->get_logger(),
+      "Similarity : ->%s<-", robot.name.c_str());
+    if (request->robotname == robot.name){
+      response->validname = true;
+      response->location = locations[robot.roomNumber];
+      response->busy = robot.busy;
+      response->batterylevel = robot.batterylevel;
+      gotIt = true;
+      break;
+    }
+  }
+  if (gotIt == false){
+    RCLCPP_INFO(
+      g_node->get_logger(),
+      "No entry for %s in robot data base", request->robotname.c_str());
+  }
 }
 
 int main(int argc, char ** argv)
